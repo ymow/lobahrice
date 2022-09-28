c51dba0e787c939fb7c8f32884ef5327353723a5
Ymow Wu <kenshin0812@hotmail.com>
2022-09-28 15:01:39 +0800

    initinalize project and integrate with naive vue framework

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import { defineNuxtConfig } from 'nuxt'
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    build: {
+        transpile: ['vueuc'],   // fix dev error: Cannot find module 'vueuc'
+    },
+    vite: {
+        plugins: [
+            Components({
+                resolvers: [NaiveUiResolver()], // Automatically register all components in the `components` directory
+            }),
+        ],
+        // @ts-expect-error: Missing ssr key
+        ssr: {
+            noExternal: ['moment', 'naive-ui', '@juggle/resize-observer', '@css-render/vue3-ssr'],
+        },
+    }
+})
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { NConfigProvider, GlobalThemeOverrides } from 'naive-ui'
+  
+    const themeOverrides: GlobalThemeOverrides = {
+      common: {
+        primaryColor: '#FF0000'
+      },
+      Button: {
+        textColor: '#FF0000'
+      }
+    }
+  
+    // ...
+  </script>
+  
+  <template>
+    <n-config-provider :theme-overrides="themeOverrides">
+      <my-app />
+    </n-config-provider>
+  </template>
